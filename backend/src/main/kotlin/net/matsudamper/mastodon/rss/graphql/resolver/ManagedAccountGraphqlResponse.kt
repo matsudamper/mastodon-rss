d1ac1e4b63c71381371b62fecd21766835963385
matsudamper <let@matsudamper.net>
2026-08-16 22:52:23 +0900

    不要コメントを削除

--- backend/src/main/kotlin/net/matsudamper/mastodon/rss/graphql/resolver/ManagedAccountGraphqlResponse.kt
+++ backend/src/main/kotlin/net/matsudamper/mastodon/rss/graphql/resolver/ManagedAccountGraphqlResponse.kt
@@ -3,9 +3,6 @@ package net.matsudamper.mastodon.rss.graphql.resolver
 import net.matsudamper.mastodon.rss.graphql.model.QlAdminAccount
 import net.matsudamper.mastodon.rss.logic.AccountService
 
-/**
- * 一覧と追加の両方が同じ形のアカウントを返すので、組み立てはここに置く。
- */
 internal fun AccountService.ManagedAccount.toGraphqlResponse(): QlAdminAccount = QlAdminAccount(
     username = urls.username,
     // acct の頭の `acct:` は Mastodon の検索窓に貼るときに要らない
