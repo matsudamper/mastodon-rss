50867833542bb9f08e29fef48b970106ea33510e
matsudamper <let@matsudamper.net>
2026-08-16 18:16:57 +0900

    Apply suggestion from @matsudamper

--- frontend/src/commonMain/kotlin/net/matsudamper/mastodon/rss/frontend/api/GraphQlClient.kt
+++ frontend/src/commonMain/kotlin/net/matsudamper/mastodon/rss/frontend/api/GraphQlClient.kt
@@ -3,10 +3,6 @@ package net.matsudamper.mastodon.rss.frontend.api
 import com.apollographql.apollo.ApolloClient
 import net.matsudamper.mastodon.rss.shared.GRAPHQL_PATH
 
-/**
- * 口は管理画面に限らないので、画面ごとの API ではなくここが持つ。
- * 画面を閉じても閉じない
- */
 object GraphQlClient {
     val apollo: ApolloClient =
         ApolloClient
